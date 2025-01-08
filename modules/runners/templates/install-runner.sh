a18e618d565f6469f4bf345588e36a45f0a01a51
Matt Godbolt <matt@godbolt.org>
2025-01-08 15:38:17 -0600

    Revert "things"
    This reverts commit c53cac9613b7c7e4ca24a2df81f4ffc92e4bffed.

--- modules/runners/templates/install-runner.sh
+++ modules/runners/templates/install-runner.sh
@@ -47,7 +47,7 @@ echo OS: $os_id
 
 # Install libicu60 for arm64 on non-ubuntu
 if [[ "$architecture" == "arm64" ]] && [[ ! "$os_id" =~ ^ubuntu.* ]]; then
-  apt install -y libicu60
+  yum install -y libicu60
 fi
 
 # Install dependencies for ubuntu
